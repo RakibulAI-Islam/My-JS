34d5ec4b2923c7aa7bf198cdef6441e4864cf407
Rakibul_AI <rakib.ice07@gmail.com>
2026-06-13 20:42:49 +0600

    'ADD' '1.JS/Map-forEach.js'.

---
+++ Map-forEach.js
@@ -0,0 +1,13 @@
+// Create a Map
+const fruits = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+let text = "";
+fruits.forEach (function(value, key) {
+  text += key + ' = ' + value + '\n';
+})
+
+console.log(text);
