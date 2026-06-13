ebf1f9128745aa84c64ca83defe72a94a15230c4
Rakibul_AI <rakib.ice07@gmail.com>
2026-06-14 05:14:05 +0600

    'ADD' '1.My-JS/toCelsius.js'.

---
+++ toCelsius.js
@@ -0,0 +1,9 @@
+// Convert Fahrenheit to Celsius:
+function toCelsius(fahrenheit) {
+  return (5/9) * (fahrenheit-32);
+}
+
+// Call the toCelcius() function
+let value = toCelsius(77);
+
+console.log(value);
