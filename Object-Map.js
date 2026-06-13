5addacefa26c1f2963d5c26066f0bef0fc7caa7b
Rakibul_AI <rakib.ice07@gmail.com>
2026-06-13 22:11:09 +0600

    'ADD' '1.My-JS/Object-Map.js'.

---
+++ Object-Map.js
@@ -0,0 +1,11 @@
+const apples = { name: "Apples"};
+const bananas = { name: "Bananas"};
+const oranges = { name: "Oranges"}; 
+
+// Create a Map.
+const fruits = new Map();
+fruits.set(apples, 500);
+fruits.set(bananas, 300);
+fruits.set(oranges, 200);
+
+console.log(fruits.get(apples)); // 500
