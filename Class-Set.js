33523f078a62c4005a5f9f42c6b7b5eaba141c1a
Rakibul_AI <rakib.ice07@gmail.com>
2026-06-16 12:42:24 +0600

    'ADD' '1.My-JS/Class-Set.js'.

---
+++ Class-Set.js
@@ -0,0 +1,15 @@
+class Car {
+  constructor(brand) {
+    this._carname = brand;
+  }
+  get carname() {
+    return this._carname;
+  }
+  set carname(x) {
+    this._carname = x;
+  }
+}
+
+const myCar = new Car("Ford");
+
+console.log(myCar.carname);
